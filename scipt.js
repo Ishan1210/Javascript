dac6e7631f8f9864abf79e85723fa4fa27682fbd
Ishan1210 <ishangrover121005@gmail.com>
2024-12-12 15:31:54 +0530

    JavaScript

---
+++ scipt.js
@@ -0,0 +1,92 @@
+// function sayHello(){alert("Hello World")}
+// // document.write("This is my first page")
+// document.write("<br>");
+// document.write("<br>");
+// var pi=3.14;
+// var person="are you God";
+// var answer='yes I am!';
+// document.write(pi+"<br>");
+// document.write(person+"<br>");
+// document.write(answer+"<br>");
+// function myfunction()
+// {
+//     var carname="Welcome"
+//     document.getElementById("demo").innerHTML=carname;
+
+// }
+// function myfunction(){
+// var num=15
+// console.log(num)
+// console.log(typeof(num))
+// num=false
+// console.log(num)
+// }
+// var person={
+//     name: Ramesh;
+//     age:30;
+//     isMarried : true;
+//     address:{
+//         street : 'Vijay Nagar'
+//         FlatNo : 201
+//     };
+// };
+// console.log(person);
+
+// var person2= new Object()
+// person2.name="Ishan";
+// person2.age=32;
+// person2.isMarried=true;
+// person2.address={};
+// person2["address"]["Flatno."]=33;
+// console.log(person2);
+// }
+
+// function myfunction(){
+//     var CarName="Volvo";
+//     document.getElementById("demo1").innerHTML=typeof CarName +""+ CarName;
+// }
+// myfunction();
+// document.getElementById("demo2").innerHTML=typeof CarName;
+
+// var locales={
+//     europe:function(){
+//         var myFriend= "Monique";
+//         var france=function(){
+//             var paris=function(){
+//                 console.log(myFriend);
+//             };
+//             paris();
+//         };
+//         france();
+//     }
+// };
+//  function show(){
+//     var a="ais a local outer variable"
+//  }
+
+// var name = "John";
+// function first(){
+//     var a = "Hello";
+//     second();
+//     var x= a+ name;
+//     console.log(x)
+// }
+// function second(){
+//     var b = "Hi";
+//     third();
+//     var z= b+ name;
+//     console.log(z)
+// }
+// function third(){
+//         var c = "Hey";
+//         var z= c+ name;
+//         console.log(z)
+// }
+// first();
+var age=20;
+if(age>18){
+    document.write("<b>You are elibible to vote</b>")
+}
+
+
+
